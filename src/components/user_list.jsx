90d670157ba3279950c2e8bb45b85920178ce989
ck <thecurrencykid@gmail.com>
2016-10-08 20:04:05 -0400

    refactor

--- src/components/user_list.jsx
+++ src/components/user_list.jsx
@@ -2,24 +2,27 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions'; 
 
-class UserList extends Component{ componentWillMount(){
+class UserList extends Component{ 
+  componentWillMount(){
     this.props.getUsers(); 
   }
 
-  renderUser(user){
-    return(
-      <div className="card card-block"> 
-        <h4 className="card-title">{user.name}</h4> 
-        <p className="card-text">Place of work</p> 
-        <a className="btn btn-primary"> Email </a> 
-      </div> 
-    )
+  renderUsers(){
+    return this.props.users.map((user) => {
+      return(
+        <div className="card card-block"> 
+          <h4 className="card-title">{user.name}</h4> 
+          <p className="card-text">Place of work</p> 
+          <a className="btn btn-primary"> Email </a> 
+        </div> 
+      )
+    });
   } 
 
   render(){
     return(
       <div> 
-        {this.props.users.map(this.renderUser)} 
+        {this.renderUsers()} 
       </div> 
     );
   }
